ee450535c08c91eb1a90af143e3730217508b575
DennisMeide <66964901+DennisMeide@users.noreply.github.com>
2021-08-09 22:43:46 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	var answer = window.confirm("Diese Website ist für PCs angepasst. Willst du die Smartphone-Version nutzen?");
+	var answer = window.confirm("Diese Website ist fuer PCs angepasst. Willst du die Smartphone-Version nutzen?");
 if (answer) {
     window.location.replace('https://google.com/');
 }
